b7238842c5dc757961af56d1d495e407108e5347
Ifeoluwa <ifeoluwaolugbode2402@gmail.com>
2025-09-08 15:54:17 +0100

    commit 4

--- john.js
+++ john.js
@@ -13,7 +13,7 @@ toggle.addEventListener('click', () => {
     }
 })
 
-var modal = document.getElementById("id01");
+const modal = document.getElementById("id01");
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
